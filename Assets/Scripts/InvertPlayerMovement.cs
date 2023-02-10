6b7f3ddace069ca3bd4b4120a7faa95fb2bd1bf5
Ignut <mister.strays@gmail.com>
2023-02-10 17:06:05 +0300

    Initial commit

---
+++ Assets/Scripts/InvertPlayerMovement.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertPlayerMovement : MonoBehaviour
+{
+    public float speed = 7f;
+    
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        
+        transform.position = new Vector3(transform.position.x - moveX, transform.position.y - moveY);
+    }
+}
